13edfb5b10f195e4c740cb101ccb2a8856b57fd4
Julio <julio@fontventa.com>
2021-11-04 11:58:57 +0100

    Subida produccion v1.0.6 (cambios diseño)

--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class GlobalService {
 
-  public static version = "1.0.5";
+  public static version = "1.0.6";
   public static backClicked = false;
   public static hideSplash = false;
   public static readyFired = false;
